622a5ffc7b246d2eeabdd3659c88488174f8404e
S00233102 <S00233102@LAB-B2314-14>
2024-03-11 11:13:48 +0000

    Add project files.

---
+++ Utilities/DatabaseHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjectManager.Utilities
+{
+    internal class DatabaseHandler
+    {
+        ProjectDBEntities db = new ProjectDBEntities();
+
+        //Projects Table
+        public List<Project> FetchProjects()
+        {
+            var query = from project in db.Projects
+                        select project;
+
+            return query.ToList();
+        }
+
+        public void CreateProject(Project newProject)
+        {
+            try
+            {
+                db.Projects.Add(newProject);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Tasks Table
+    }
+}
